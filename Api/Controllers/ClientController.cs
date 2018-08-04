909ac530cd25b80404cbf39aafad5842d1be6152
danny <burrows.danny@gmail.com>
2018-08-04 15:27:55 -0500

    Client

---
+++ Api/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PactNet.Library;
+
+namespace Api.Controllers {
+  [Route ("api/[controller]")]
+  [ApiController]
+  public class ClientController : ControllerBase {
+    private PactNetClient _client = new PactNetClient();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<object>> Get () {
+      return Ok (new List<object> {
+        _client.Get()
+      });
+    }
+  }
+}
